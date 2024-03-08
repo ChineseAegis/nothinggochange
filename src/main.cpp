5340a87b511718607e32eaec06bfe582b8432e61
LH <1962937337@qq.com>
2024-03-08 14:11:56 +0000

    lh

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
-// #include <iostream>
-// #include"Map.hpp"
-// using namespace std;
+#include <iostream>
+#include"Map.hpp"
+using namespace std;
 
 int main(int argc, char **argv)
 {
     Map map;
     cout << "hello world!" << endl;
     return 0;
-    //;
-    // nihaod
-    //2
-    //1
 }
 
