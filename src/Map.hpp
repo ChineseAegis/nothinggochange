bb3dbf5a8ac10f36f88c78896840e2f800e6f459
LH <1962937337@qq.com>
2024-03-08 14:06:49 +0000

    lh

--- src/Map.hpp
+++ src/Map.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 // 处理地图，存储地图数据
 class Map
 {
@@ -19,18 +20,34 @@ class Map
 public:
     static const int WIDTH = 200;
     static const int HEIGHT = 200;
-    //二维数组，代表地图中每个坐标的元素
+    // 二维数组，代表地图中每个坐标的元素
     std::vector<std::vector<char>> grid;
 
     Map()
     {
-        std::cout<<"Map constructing"<<std::endl;
     }
 
-
     // 读取文件，将元素放入grid
-    void readfile(std::string filename)
+    void readfile(const std::string filename)
     {
-      
+        std::ifstream file(filename); // 打开文件
+        if (!file.is_open())
+        {
+            std::cerr << "无法打开文件  ";
+            return;
+        }
+
+        std::string line;
+        while (std::getline(file, line))
+        { // 逐行读取
+            std::vector<char> row;
+            for (char c : line)
+            {                     // 遍历行中的每个字符
+                row.push_back(c); // 添加到当前行的 vector 中
+            }
+            grid.push_back(row); // 将当前行添加到 grid 中
+        }
+
+        file.close(); // 关闭文件
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-#include"Map.hpp"
-using namespace std;
+// #include <iostream>
+// #include"Map.hpp"
+// using namespace std;
 
-int main(int argc, char **argv)
-{
-    Map map;
-    cout << "hello world!" << endl;
-    return 0;
-    // nihaod
-    //2
-    //1
-}
+// int main(int argc, char **argv)
+// {
+//     Map map;
+//     cout << "hello world!" << endl;
+//     return 0;
+//     // nihaod
+//     //2
+//     //1
+// }
 
